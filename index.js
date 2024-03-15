38eb0515eaad4363a181a56ef894f8cc5cc072b7
suwathi <suwathi.saw@gmail.com>
2024-03-15 11:27:17 +0530

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.SOCKET_IO_ORIGIN || "http://localhost:3000", // Use environment variable or fallback to "http://localhost:3000"
     methods: ["GET", "POST"],
   },
 });
